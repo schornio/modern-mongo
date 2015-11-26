8ccdb0839abc9232df9af598b3363e0269237d19
Thomas Schorn <thomas@schorn.io>
2015-11-26 16:12:14 +0100

    Applied private project

---
+++ test/collection.spec.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const TEST_DB = process.env.TEST_DB;
+const COLLECTION_NAME = 'test';
+
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-as-promised'));
+
+const mongodb = require('mongodb');
+
+describe('MongoDB Collection', function () {
+  const Document = require('../lib/document');
+  const Collection = require('../lib/collection');
+
+  let db;
+  let testCollection;
+
+  class TestDocument extends Document {
+    constructor(db) {
+      super(db);
+    }
+  }
+
+  class TestCollection extends Collection {
+    constructor(db) {
+      super(db, TestDocument, COLLECTION_NAME);
+    }
+  }
+
+  before(function () {
+    chai.assert.ok(TEST_DB, 'Environment: TEST_DB');
+    return mongodb.MongoClient
+      .connect(TEST_DB)
+      .then((connectedDb) => db = connectedDb)
+      .then(() => testCollection = new TestCollection(db));
+  });
+
+  afterEach(function () {
+    return db.collection(COLLECTION_NAME).remove({});
+  });
+
+  it('should create new document instances', function () {
+
+    let newTestDocument = testCollection.new();
+
+    expect(newTestDocument).to.be.an.instanceOf(TestDocument);
+  });
+
+  it('should find one document in collection', function () {
+    let bareTestDocument = { _id: 1, message: 'Hello World' };
+    return db.collection(COLLECTION_NAME)
+      .insert(bareTestDocument)
+      .then(() => {
+        return expect(testCollection.findOne())
+          .to.eventually.be.an.instanceOf(TestDocument)
+          .and.to.be.deep.equal(bareTestDocument);
+      });
+  });
+
+  it('should find many documents in collection', function () {
+    let bareTestDocuments = [
+      { _id: 1, message: 'Hello World 1' },
+      { _id: 2, message: 'Hello World 2' },
+      { _id: 3, message: 'Hello World 3' }
+    ];
+    return db.collection(COLLECTION_NAME)
+      .insert(bareTestDocuments)
+      .then(() => {
+        return expect(testCollection.findMany())
+          .to.eventually.deep.equal(bareTestDocuments);
+      })
+      .then((documents) => {
+        for (let doc of documents) {
+          expect(doc).to.be.an.instanceOf(TestDocument);
+        }
+      });
+  });
+
+  it('should remove many documents in collection', function () {
+    let bareTestDocuments = [
+      { _id: 1, message: 'Hello World 1' },
+      { _id: 2, message: 'Hello World 2' },
+      { _id: 3, message: 'Hello World 3' }
+    ];
+    return db.collection(COLLECTION_NAME)
+      .insert(bareTestDocuments)
+      .then(() => {
+        return expect(testCollection.remove())
+          .to.eventually.be.ok;
+      })
+      .then(() => {
+        return expect(db.collection(COLLECTION_NAME).find().toArray())
+          .to.eventually.deep.equal([]);
+      });
+  });
+
+});
---
+++ test/document.spec.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const TEST_DB = process.env.TEST_DB;
+const COLLECTION_NAME = 'test';
+
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-as-promised'));
+
+const mongodb = require('mongodb');
+
+describe('MongoDB Document', () => {
+  const Document = require('../lib/document');
+
+  let db_collection;
+
+  class TestDocument extends Document {
+    constructor(db_collection) {
+      super(db_collection);
+    }
+  }
+
+  before(() => {
+    chai.assert.ok(TEST_DB, 'Environment: TEST_DB');
+    return mongodb.MongoClient
+      .connect(TEST_DB)
+      .then((connectedDb) => db_collection = connectedDb.collection(COLLECTION_NAME));
+  });
+
+  afterEach(() => {
+    return db_collection.remove({});
+  });
+
+  it('should save new document', () => {
+    let newTestDocument = new TestDocument(db_collection);
+    newTestDocument.message = 'Hallo Welt!';
+
+    return expect(newTestDocument.save().then(() => db_collection.findOne()))
+      .to.eventually.deep.equal(newTestDocument);
+  });
+
+  it('should save existing document', () => {
+    let bareTestDocument = {
+      _id: 1,
+      message: 'Hallo Welt'
+    };
+    let newTestDocument = new TestDocument(db_collection);
+    newTestDocument.apply(bareTestDocument);
+
+    return expect(db_collection
+      .insert(bareTestDocument)
+      .then(() => newTestDocument.save())
+      .then(() => db_collection.findOne())
+    ).to.eventually.deep.equal(newTestDocument);
+  });
+
+  it('should apply properties from bare object', () => {
+    let testDocument = new TestDocument(db_collection);
+
+    testDocument.propertyA = 'propA';
+    testDocument.propertyB = 'propB';
+
+    testDocument.apply({
+      propertyB: 'appliedPropB',
+      propertyC: 'propC'
+    });
+
+    expect(testDocument)
+      .to.deep.equal({
+        _id: testDocument._id,
+        propertyA: 'propA',
+        propertyB: 'appliedPropB',
+        propertyC: 'propC'
+      });
+  });
+
+  it('should validate document', () => {
+    let testDocument = new TestDocument(db_collection);
+
+    expect(testDocument.validate()).to.be.equal(true);
+
+    delete testDocument._id;
+
+    expect(testDocument.validate()).to.be.equal(false);
+  });
+
+});
