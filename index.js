29a4fa1d294faaa461c70612e9a42387d6896332
Thomas Schorn <thomas@schorn.io>
2015-12-01 17:57:35 +0100

    Versioned Document

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const mongodb = require('mongodb');
 module.exports = {
   Document: require(__dirname + '/lib/document'),
   Collection: require(__dirname + '/lib/collection'),
+  VersionedDocument: require(__dirname + '/lib/versioned_document'),
   connect: mongodb.MongoClient.connect
 };
---
+++ test/versioned_document.spec.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const TEST_DB = process.env.TEST_DB;
+const COLLECTION_NAME = 'test';
+
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-as-promised'));
+
+const mm = require('../index');
+
+describe('Versioned MongoDB Document', () => {
+  let db_collection;
+  let db_collection_history;
+
+  class TestDocument extends mm.VersionedDocument {
+    constructor(db_collection, db_collection_history) {
+      super(db_collection, db_collection_history);
+    }
+  }
+
+  before(() => {
+    chai.assert.ok(TEST_DB, 'Environment: TEST_DB');
+    return mm
+      .connect(TEST_DB)
+      .then((connectedDb) => {
+         db_collection = connectedDb.collection(COLLECTION_NAME);
+         db_collection_history = connectedDb.collection(COLLECTION_NAME + '_history');
+       });
+  });
+
+  afterEach(() => {
+    return Promise.all([
+      db_collection.deleteMany({}),
+      db_collection_history.deleteMany({})
+    ]);
+  });
+
+  it('should save new document - versioned', () => {
+    let newTestDocument = new TestDocument(db_collection, db_collection_history);
+    newTestDocument.message = 'Hallo Welt!';
+
+    return expect(newTestDocument.save().then(() => db_collection.findOne()))
+      .to.eventually.deep.equal(newTestDocument)
+      .and.to.have.property('_v', 1)
+      .then(() => {
+        return expect(db_collection_history.find().toArray())
+          .to.eventually.be.deep.equal([]);
+      })
+      .then(() => {
+        newTestDocument.message = 'Hallo schoene Welt!';
+        return expect(newTestDocument.save().then(() => db_collection.findOne()))
+          .to.eventually.deep.equal(newTestDocument)
+          .and.to.have.property('_v', 2);
+      })
+      .then(() => {
+        return expect(db_collection_history.find().toArray())
+          .to.eventually.satisfy((docs) => {
+            let doc = docs[0];
+
+            expect(docs.length).to.be.equal(1);
+            expect(doc.doc_id.equals(newTestDocument._id)).to.be.equal(true);
+            expect(doc._v).to.be.equal(1);
+            expect(doc.message).to.be.equal('Hallo Welt!');
+
+            return true;
+          });
+      });
+  });
+
+  it('should save existing document', () => {
+    let bareTestDocument = {
+      _id: 1,
+      _v: 1,
+      message: 'Hallo Welt'
+    };
+    let newTestDocument = new TestDocument(db_collection, db_collection_history);
+    newTestDocument.apply(bareTestDocument);
+
+    return expect(db_collection
+      .insert(bareTestDocument)
+      .then(() => newTestDocument.save())
+      .then(() => newTestDocument.save())
+      .then(() => newTestDocument.save())
+      .then(() => db_collection.find().toArray())
+    ).to.eventually.deep.equal([newTestDocument])
+    .then(() => {
+      return expect(db_collection_history.find().toArray())
+        .to.eventually.have.property('length', 3);
+    });
+  });
+
+  it('should apply properties from bare object', () => {
+    let testDocument = new TestDocument(db_collection, db_collection_history);
+
+    testDocument.propertyA = 'propA';
+    testDocument.propertyB = 'propB';
+
+    testDocument.apply({
+      propertyB: 'appliedPropB',
+      propertyC: 'propC'
+    });
+
+    expect(testDocument)
+      .to.deep.equal({
+        _id: testDocument._id,
+        propertyA: 'propA',
+        propertyB: 'appliedPropB',
+        propertyC: 'propC'
+      });
+  });
+
+  it('should validate document', () => {
+    let testDocument = new TestDocument(db_collection, db_collection_history);
+
+    expect(testDocument.validate()).to.be.equal(true);
+
+    delete testDocument._id;
+
+    expect(testDocument.validate()).to.be.equal(false);
+  });
+
+  it('should reject on same version is inserted twice', function () {
+    let bareTestDocument = {
+      _id: 1,
+      _v: 1,
+      message: 'Hallo Welt'
+    };
+    let newTestDocument = new TestDocument(db_collection, db_collection_history);
+    newTestDocument.apply(bareTestDocument);
+
+    return expect(db_collection
+      .insert(bareTestDocument)
+      .then(() => newTestDocument.save())
+      .then(() => newTestDocument.save())
+      .then(() => db_collection.update({ _id: 1 }, { $set: { _v: 1 } }))
+      .then(() => newTestDocument.save())
+    ).to.be.rejectedWith(/Cannot save new version/);
+  });
+
+});
